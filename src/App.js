1efb3130c71f4febd847963535a26b86e45da10c
Parth Patel <parth_patel@comcast.com>
2021-06-16 17:32:38 -0400

    create smaller component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,35 @@
-import React, { Component } from 'react';
+import React from 'react';
+
+import Expenses from './components/Expenses';
 
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
+function App() {
+
+  const expenses = [
+    {
+      date: new Date(2021,5,21),
+      title: "Car rent",
+      price: "$250.00"
+    },
+    {
+      date: new Date(2021,4,30),
+      title: "House rent",
+      price: "$1050.00"
+    },
+    {
+      date: new Date(2021,6,10),
+      title: "Car Payment",
+      price: "$450.00"
+    }
+  ];
+
+  return (
       <div className="App">
           <h1 className="App-title">Welcome to React</h1>
+          <Expenses expenses={expenses} />
       </div>
     );
   }
-}
 
 export default App;
---
+++ src/components/Expenses.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ExpenseItem from './ExpenseItem';
+
+import './Expenses.css';
+
+function Expenses(props) {
+    return (
+        <div className="expenses">
+            <ExpenseItem title={props.expenses[0].title} date={props.expenses[0].date} price={props.expenses[0].price} />
+            <ExpenseItem title={props.expenses[1].title} date={props.expenses[1].date} price={props.expenses[1].price} />
+            <ExpenseItem title={props.expenses[2].title} date={props.expenses[2].date} price={props.expenses[2].price} />
+        </div>
+    )
+}
+
+export default Expenses;
