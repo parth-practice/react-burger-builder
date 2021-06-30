784e8decb1ecf146530543bda379bd0d448778ac
Parth Patel <parth_patel@comcast.com>
2021-06-29 20:38:53 -0400

    create empty wrapper

--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -1,15 +1,17 @@
 import React from 'react';
+
 import ExpenseItem from './ExpenseItem';
+import Card from './Card';
 
 import './Expenses.css';
 
 function Expenses(props) {
     return (
-        <div className="expenses">
+        <Card className="expenses">
             <ExpenseItem title={props.expenses[0].title} date={props.expenses[0].date} price={props.expenses[0].price} />
             <ExpenseItem title={props.expenses[1].title} date={props.expenses[1].date} price={props.expenses[1].price} />
             <ExpenseItem title={props.expenses[2].title} date={props.expenses[2].date} price={props.expenses[2].price} />
-        </div>
+        </Card>
     )
 }
 
